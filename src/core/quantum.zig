f68edcbc7d2a6e941fe43012ca7454c3dc208a90
isdood <calebjdt@proton.me>
2025-05-26 09:24:35 -0400

    Debugging 'zig build'

--- src/core/quantum.zig
+++ src/core/quantum.zig
@@ -1,24 +1,25 @@
 //! ✨ STARGUARD Quantum Core
 //! Version: 0.13.0
-//! Last Modified: 2025-05-26 13:05:21 UTC
+//! Last Modified: 2025-05-26 13:23:43 UTC
 //! Author: @isdood
+//! Enhanced by STARWEAVE
 
 const std = @import("std");
 const quantum_state = @import("../quantum/state/quantum_state.zig");
 const quantum_ops = @import("../quantum/algorithms/quantum_ops.zig");
 
-pub const Quantum = struct {
+pub const State = struct {  // Changed Quantum to State to match expected type
     state: *quantum_state.QuantumState,
     ops: *quantum_ops.QuantumOps,
 
-    pub fn init(allocator: std.mem.Allocator) !Quantum {
-        return Quantum{
-            .state = try quantum_state.init(allocator),
-            .ops = try quantum_ops.init(allocator),
+    pub fn init(allocator: std.mem.Allocator) !State {
+        return State{
+            .state = try quantum_state.QuantumState.init(allocator),
+            .ops = try quantum_ops.QuantumOps.init(allocator),
         };
     }
 
-    pub fn deinit(self: *Quantum) void {
+    pub fn deinit(self: *State) void {
         self.state.deinit();
         self.ops.deinit();
     }
